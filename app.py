fc8dc975107031de4f9d2640106b7d973adc26d9
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2021-10-27 19:01:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,6 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from flask.wrappers import Response
 import wikipedia
 from wikipedia.exceptions import PageError
-# chatterbot==1.0.8 has issue with spacy v3 so got to edit tagging.py's 13th line for english support
-#It's only for the first time
-#Sorry for my trashy code but it works :)
-with open(r'venv\Lib\site-packages\chatterbot\tagging.py', 'r') as tagfile:
-    # read a list of lines into myeditlines
-    myeditlines = tagfile.readlines()
-
-# now change the 13th line, note that you have to add a newline
-myeditlines[12] = ("   "+"     if self.language.ISO_639_1.lower() == 'en':\n"+"   "+"         self.nlp = spacy.load('en_core_web_sm')\n"+"   "+"     else:\n"+"  "+"          self.nlp = spacy.load(self.language.ISO_639_1.lower())\n")
-
-# and write everything back
-with open(r'venv\Lib\site-packages\chatterbot\tagging.py', 'a') as tagfile:
-    tagfile.writelines(myeditlines)
-    tagfile.close()
-
 
 app = Flask(__name__)
 #create chatbot
