1d074cd3df2584400f8f598582798057ee3e2638
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2021-10-28 11:58:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from flask.wrappers import Response
+from flask_sqlalchemy import SQLAlchemy
 import wikipedia
 from wikipedia.exceptions import PageError
 #import nltk
