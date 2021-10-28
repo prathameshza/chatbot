b024ee9a6825016e6e79b7fb340611210f74615a
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2021-10-28 11:29:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from flask.wrappers import Response
 import wikipedia
 from wikipedia.exceptions import PageError
-import nltk
-nltk.download('punkt')
+#import nltk
+#nltk.download('punkt')
 
 app = Flask(__name__)
 #create chatbot
