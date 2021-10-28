5c9b5d6214324f7b1579629445631a0fcb401b76
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2021-10-28 12:18:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def get_bot_response():
         statement=str(englishBot.get_response(userText))
     return statement
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+#@app.before_first_request
+#def create_tables():
+#    db.create_all()
 
 if __name__ == "__main__":
     app.run()
