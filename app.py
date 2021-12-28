85047ef88ea4aa5a24e3fd9df3a46cb7721e6e86
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2021-12-28 11:59:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,17 @@ trainer = ChatterBotCorpusTrainer(englishBot)
 trainer.train("chatterbot.corpus.english")
 read_only=True
 
+def fixBadZipfile(zipFile):  
+ f = open(zipFile, 'r+b')  
+ data = f.read()  
+ pos = data.find('b\x50\x4b\x05\x06') # End of central directory signature  
+ if (pos > 0):  
+     self._log("Trancating file at location " + str(pos + 22)+ ".")  
+     f.seek(pos + 22)   # size of 'ZIP end of central directory record' 
+     f.truncate()  
+     f.close()  
+ else:  
+     raise error, file is truncated  
 
 #define app routes
 @app.route("/")
